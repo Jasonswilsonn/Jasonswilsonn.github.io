7d3315c0e522808936d7ed2894cfa606de86e1de
Jasonswilsonn <92189073+Jasonswilsonn@users.noreply.github.com>
2023-10-11 20:09:58 +0100

    initial upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+const hiddenElements = document.querySelectorAll('.hidden')       /* learnt from https://www.youtube.com/watch?v=huVJW23JHKQ&t=695s&ab_channel=KevinPowell */
+
+const observer = new IntersectionObserver((entries, ) => {
+  entries.forEach((entry) => {
+    entry.target.classList.toggle("show", entry.isIntersecting)
+  })
+  }
+)
+
+
+hiddenElements.forEach(hidden => { observer.observe(hidden)
+});
+
+
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]        /* learnt from https://www.youtube.com/watch?v=At4B7A4GOPg */
+const navbtn = document.getElementsByClassName('nav-btn-container')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbtn.classList.toggle('active')
+})
