bb7f2577d1173a34f7a34f3b4d33c45063336d9e
Jasonswilsonn <92189073+Jasonswilsonn@users.noreply.github.com>
2023-10-15 01:57:39 +0100

    markup summarys and navbar colour changes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 
+/* slide in */
 const hiddenElements = document.querySelectorAll('.hidden')       /* learnt from https://www.youtube.com/watch?v=huVJW23JHKQ&t=695s&ab_channel=KevinPowell */
 
 const observer = new IntersectionObserver((entries, ) => {
@@ -13,7 +14,7 @@ hiddenElements.forEach(hidden => { observer.observe(hidden)
 });
 
 
-
+/* navbar */
 const toggleButton = document.getElementsByClassName('toggle-button')[0]        /* learnt from https://www.youtube.com/watch?v=At4B7A4GOPg */
 const navbtn = document.getElementsByClassName('nav-btn-container')[0]
 
